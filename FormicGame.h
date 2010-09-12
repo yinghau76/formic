5d46ed8ff71062b6859a8c097928f54f48a594b4
Patrick Tsai <yinghau76@gmail.com>
2010-09-12 20:09:19 +0800

    + Add view and model

---
+++ FormicGame.h
@@ -0,0 +1,16 @@
+//
+//  FormicGame.h
+//  Formic
+//
+//  Created by patrick on 2010/9/12.
+//  Copyright 2010 Patrick Tsai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface FormicGame : NSObject {
+
+}
+
+@end
---
+++ FormicGame.m
@@ -0,0 +1,14 @@
+//
+//  FormicGame.m
+//  Formic
+//
+//  Created by patrick on 2010/9/12.
+//  Copyright 2010 Patrick Tsai. All rights reserved.
+//
+
+#import "FormicGame.h"
+
+
+@implementation FormicGame
+
+@end
